842929dae9f49a8e765610d000fe55a04654d996
Alexander Jung <AJung@o365.comhard.de>
2025-03-28 09:14:48 +0000

    Implement POST functionality to create new TODO items and update todo-liste.html with a form for input

--- todo-api.php
+++ todo-api.php
@@ -29,8 +29,17 @@ switch ($_SERVER['REQUEST_METHOD']) {
         write_log("GET", $todo_items);
         break;
     case 'POST':
-        // Placeholder for creating a new TODO
-        break;
+            // Get data from the input stream.
+            $data = json_decode(file_get_contents('php://input'), true);
+            // Create new todo item.
+            $new_todo = ["id" => uniqid(), "title" => $data['title']];
+            // Add new item to our todo item list.
+            $todo_items[] = $new_todo;
+            // Write todo items to JSON file.
+            file_put_contents($todo_file, json_encode($todo_items));
+            // Return the new item.
+            echo json_encode($new_todo);
+    break;
     case 'PUT':
         // Placeholder for updating a TODO
         break;
