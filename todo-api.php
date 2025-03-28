15a7a9de0bc0858511c7f659251b43f93b33ab8a
Alexander Jung <AJung@o365.comhard.de>
2025-03-28 07:57:13 +0000

    created api and a first test log

--- todo-api.php
+++ todo-api.php
@@ -1,6 +1,17 @@
 <?php
 header('Content-Type: application/json');
  
+// LOG function in PHP
+function write_log($action, $data) {
+    $log = fopen('log.txt', 'a');
+    $timestamp = date('Y-m-d H:i:s');
+    fwrite($log, "$timestamp - $action: " . json_encode($data) . "\n");
+    fclose($log);
+}
+
+write_log("init", ["empty"]);
+
+
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'GET':
         // Placeholder for reading TODO items
