f6470b67268796ad7d6ac3ae15ce3fa5eec911f6
Alexander Jung <AJung@o365.comhard.de>
2025-03-28 07:40:32 +0000

    Create file skeletons for the CRUD operations

---
+++ todo-api.php
@@ -0,0 +1,18 @@
+<?php
+header('Content-Type: application/json');
+ 
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'GET':
+        // Placeholder for reading TODO items
+        break;
+    case 'POST':
+        // Placeholder for creating a new TODO
+        break;
+    case 'PUT':
+        // Placeholder for updating a TODO
+        break;
+    case 'DELETE':
+        // Placeholder for deleting a TODO
+        break;
+}
+?>
---
+++ todo.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+ 
+    // Define the URL to our CRUD server api
+    const apiUrl = 'todo-api.php';
+ 
+});
