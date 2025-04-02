9b735c3f4b0fd6f1f4adbee4e2a6aa8c3f157c55
Alexander Jung <AJung@o365.comhard.de>
2025-04-02 00:37:26 +0000

    Add documentation comments to test-mysql.php for connection testing

--- test-mysql.php
+++ test-mysql.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * test-mysql.php – Verbindungstest zur MySQL-Datenbank
+ *
+ * Dieses Skript stellt eine Verbindung zur MySQL-Datenbank "test_db" her
+ * und überprüft, ob die Verbindung erfolgreich war.
+ *
+ * Es verwendet die objektorientierte MySQLi-Erweiterung.
+ * Bei erfolgreicher Verbindung wird eine entsprechende Erfolgsmeldung ausgegeben,
+ * andernfalls wird eine Fehlermeldung angezeigt und das Skript beendet.
+ *
+ * Dieses Skript dient ausschließlich zu Testzwecken und sollte
+ * nicht im Produktivbetrieb verwendet werden.
+ */
 $mysqli = new mysqli("localhost", "alexjung", "links234", "test_db");
  
 if ($mysqli->connect_error) {
