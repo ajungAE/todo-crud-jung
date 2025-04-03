82caa10449a17229e8357e53f70a6b3bda722768
Alexander Jung <AJung@o365.comhard.de>
2025-04-03 13:49:54 +0000

    Add CRUD methods to TodoDB class for managing todo items

--- classes/TodoDB.php
+++ classes/TodoDB.php
@@ -1,21 +1,24 @@
 <?php
+
 /**
-* Todo list database object.
-*
-* Global variable with the object of our TodoDB class.
-*/
+ * Todo list database object.
+ *
+ * Global variable with the object of our TodoDB class.
+ */
 require_once('./logging.php');
 require_once('./config.php');
 
-$todoDB = new TodoDB();
-class TodoDB {
+
+class TodoDB
+{
     private $connection;
     private $stmt;
- 
+
     /**
      * Contructructor of the TodoDB class.
      */
-    public function __construct() {
+    public function __construct()
+    {
         global $host, $db, $user, $pass;
         try {
             $this->connection = new PDO(
@@ -24,16 +27,56 @@ class TodoDB {
                 $pass
             );
             $this->connection->setAttribute(
-                PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+                PDO::ATTR_DEFAULT_FETCH_MODE,
+                PDO::FETCH_ASSOC
+            );
         } catch (Exception $e) {
             error_log($e->getMessage());
         }
     }
 
-    public function getTodos() {
+    public function getTodos()
+    {
         $statement = $this->connection->query("SELECT * FROM todo");
         $todo_items = $statement->fetchAll();
         return $todo_items;
     }
-}
 
+    public function addTodo($title) {
+        $stmt = $this->connection->prepare(
+            "INSERT INTO todo (title, completed) VALUES (:title, :completed)"
+        );
+        $stmt->execute(['title' => $title, 'completed' => 0]);
+    
+        $id = $this->connection->lastInsertId();
+        $stmt = $this->connection->prepare("SELECT * FROM todo WHERE id = ?");
+        $stmt->execute([$id]);
+    
+        return $stmt->fetch();
+    }
+    
+
+    public function setCompleted($id, $completed) {
+        $statement = $this->connection->prepare(
+            "UPDATE todo SET completed = :completed WHERE id = :id");
+        $statement->execute(["id" => $id, "completed" => $completed]);
+    }
+
+    public function updateTodo($id, $title, $completed) {
+        $stmt = $this->connection->prepare(
+            "UPDATE todo SET title = ?, completed = ? WHERE id = ?"
+        );
+        $stmt->execute([$title, $completed, $id]);
+    
+        return ["status" => "success"];
+    }
+
+    public function deleteTodo($id) {
+        $stmt = $this->connection->prepare("DELETE FROM todo WHERE id = ?");
+        $stmt->execute([$id]);
+    
+        return ["status" => "success"];
+    }
+
+
+}
